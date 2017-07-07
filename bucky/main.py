887242126b58743d31866fd5774084c29cbdadcb
jarek <jarek.siembida@smarkets.com>
2017-07-07 19:46:01 +0100

    WIP: manager process

--- bucky/main.py
+++ bucky/main.py
@@ -49,64 +49,88 @@ def main(argv=sys.argv):
         print("Error...", file=sys.stderr)
         sys.exit(1)
 
-    source_subprocesses = []
-    destination_subprocesses = []
+    src_subprocesses, dst_subprocesses = {}, {}
 
     def shutdown(subprocesses):
         err = 0
-        for i in subprocesses:
-            cfg.log.info("Terminating %s", i.name)
-            i.terminate()
-        for i in subprocesses:
-            i.join(1)
-            if i.exitcode is None:
-                cfg.log.warning("%s still running, killing", i.name)
+        for timestamps, p, arg in subprocesses.values():
+            if p and p.exitcode is None:
+                cfg.log.info("Terminating module %s", p.name)
+                p.terminate()
+        for timestamps, p, arg in subprocesses.values():
+            p.join(1)
+            if p.exitcode is None:
+                cfg.log.warning("Module %s still running, killing", p.name)
                 err = 1
-                os.kill(i.pid, 9)
-                i.join()
+                os.kill(p.pid, 9)
+                p.join()
         return err
 
-    def startup(subprocesses):
-        for i in subprocesses:
-            cfg.log.info("Starting %s", i.name)
-            i.start()
+    def healthcheck(subprocesses):
+        def start_module(module_name, module_class, timestamps, arg):
+            now = time.time()
+            timestamps.append(now)
+            timestamps = timestamps[-10:]
+            p = module_class(module_name, config_file, arg)
+            cfg.log.info("Starting module %s", p.name)
+            p.start()
+            return timestamps, p, arg
 
-    def terminate():
-        err = shutdown(source_subprocesses)
-        err += shutdown(destination_subprocesses)
+        err = 0
+
+        for (module_name, module_class), (timestamps, p, arg) in subprocesses.items():
+            if p is None:
+                subprocesses[(module_name, module_class)] = start_module(module_name, module_class, timestamps, arg)
+                continue
+            if p.exitcode is not None:
+                cfg.log.warning("Module %s has exited, trying to recover", p.name)
+                p.join()
+                if len(timestamps) > 5:
+                    average_time_between_starts = sum(
+                        timestamps[i] - timestamps[i - 1] for i in range(1, len(timestamps))
+                    ) / (len(timestamps) - 1)
+                    if average_time_between_starts < 60:
+                        cfg.log.critical("Module %s keeps failing, cannot recover", p.name)
+                        err = 1
+                        continue
+                subprocesses[(module_name, module_class)] = start_module(module_name, module_class, timestamps, arg)
+                continue
+            cfg.log.debug("Module %s is up", p.name)
+
+        return err
+
+    def terminate(err=0):
+        err += shutdown(src_subprocesses)
+        err += shutdown(dst_subprocesses)
         sys.exit(err != 0)
 
-    def tick():
-        pass
+    common.prepare_module('bucky3', config_file, lambda: True, terminate)
 
-    common.prepare_module('bucky3', config_file, tick, terminate)
-
-    sources = []
-    destinations = []
+    src_buf, dst_buf = [], []
 
     for k, v in vars(cfg).items():
         if not k.startswith('_') and type(v) == dict and 'type' in v:
             module_name, module_type = k, v['type']
             module_class = MODULES[module_type]
             if issubclass(module_class, common.MetricsSrcProcess):
-                sources.append((module_name, module_class))
+                src_buf.append((module_name, module_class))
             elif issubclass(module_class, common.MetricsDstProcess):
-                destinations.append((module_name, module_class))
+                dst_buf.append((module_name, module_class))
             else:
                 raise ValueError("Invalid module type")
 
     pipes = []
-    for module_name, module_class in destinations:
+    for module_name, module_class in dst_buf:
         send, recv = multiprocessing.Pipe()
-        destination_subprocesses.append(module_class(module_name, config_file, recv))
+        dst_subprocesses[(module_name, module_class)] = [], None, recv
         pipes.append(send)
-    for module_name, module_class in sources:
-        source_subprocesses.append(module_class(module_name, config_file, pipes))
-
-    startup(destination_subprocesses)
-    startup(source_subprocesses)
+    for module_name, module_class in src_buf:
+        src_subprocesses[(module_name, module_class)] = [], None, pipes
 
     while True:
+        err = healthcheck(src_subprocesses) + healthcheck(dst_subprocesses)
+        if err:
+            terminate(err)
         time.sleep(10)
 
 
