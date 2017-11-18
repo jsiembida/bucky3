a35b8435ee6428996d8cb473c7bc2c46c2c7392a
jarek <jarek.siembida@gmail.com>
2017-11-18 15:19:32 +0000

    Add some validation and timestamp handling for JSON objs

--- bucky3/jsond.py
+++ bucky3/jsond.py
@@ -39,6 +39,31 @@ class JsonDServer(module.MetricsSrcProcess, module.UDPConnector):
             # TODO there is no protection against malicious / malformed lines
             obj, end = self.decoder.raw_decode(line)
             if end == len(line) and type(obj) is dict:
-                self.buffer_metric('metrics', obj, recv_timestamp, None)
+                self.handle_obj(obj)
         except JSONDecodeError as e:
             pass
+
+    def handle_obj(self, recv_timestamp, obj):
+        # Only flat objects with basic types
+        for k, v in obj.items():
+            if not isinstance(v, (int, float, bool, str, None)):
+                return
+        # Parsing ISO/RFC would be really nice, but in Python is not going to be simple
+        # and fast. So let's accept only a sensible number of secs / millis from epoch.
+        cust_timestamp = None
+        if 'timestamp' in obj:
+            cust_timestamp = obj['timestamp']
+            # TODO Here we accept custom timestamps from roughly 2000 to 2060,
+            # this is inconsistent with statsd module that only accepts those
+            # in a very small, configurable window (i.e. +/- 10mins from now).
+            if isinstance(cust_timestamp, (int, float)):
+                if 1000000000 < cust_timestamp < 3000000000:  # Looks like seconds
+                    pass
+                elif 1000000000000 < cust_timestamp < 3000000000000:  # Looks like millis
+                    cust_timestamp = cust_timestamp / 1000
+                else:
+                    cust_timestamp = None
+            else:
+                cust_timestamp = None
+            del obj['timestamp']
+        self.buffer_metric('metrics', obj, cust_timestamp or recv_timestamp, None)
